8bf51b7e8eb67a2f9d532950eccda9ef031a6148
ansaries <arslan@ansaries.com>
2017-09-27 21:19:56 +0400

    updated for custom usage
    included setWorkerUrl and other config methods in service

--- src/angular-browser-push-notifications.js
+++ src/angular-browser-push-notifications.js
@@ -1,105 +1,114 @@
 angular.module('browserPushNotifications', [])
-  .provider('BrowserPushNotifications', function() {
+  .provider('BrowserPushNotifications', function () {
     // Default values
-    var workerUrl = '/service-worker.js';
+    var workerUrl = '/sw.js';
     var userVisibleOnly = true;
 
-    // Config methods
-    this.setWorkerUrl = function(u) {
-      workerUrl = u;
-    }
 
-    this.setUserVisibleOnly = function(direction) {
-      userVisibleOnly = direction;
-    }
 
-    this.$get = ['$q', '$log', 'BrowserPushNotificationsStatus', '$rootScope', function($q, $log, BrowserPushNotificationsStatus, $rootScope) {
+    this.$get = ['$q', '$log', 'BrowserPushNotificationsStatus', '$rootScope', function ($q, $log, BrowserPushNotificationsStatus, $rootScope) {
       // All methods starting with an "_" are using ES6 promises and not Angular promises
       var service = {
-        _getPermissions: function() {
+        _getPermissions: function () {
           return Notification.permission === 'denied' ? Promise.reject(BrowserPushNotificationsStatus.USER_BLOCKED_NOTIFICATIONS) : Promise.resolve(Notification.permission);
         },
-        _getServiceWorkerRegistration: function() {
-          return navigator.serviceWorker.ready.catch(function() {
+        _getServiceWorkerRegistration: function () {
+          return navigator.serviceWorker.ready.catch(function () {
             return Promise.reject(BrowserPushNotificationsStatus.WORKER_REGISTRATION_FAIL);
           });
         },
-        _getSubscription: function(serviceWorkerRegistration) {
-          return serviceWorkerRegistration.pushManager.getSubscription().then(function(sub) {
-            if(!sub) {
-              return serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: userVisibleOnly});
+        _getSubscription: function (serviceWorkerRegistration) {
+          return serviceWorkerRegistration.pushManager.getSubscription().then(function (sub) {
+            if (!sub) {
+              return serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: userVisibleOnly });
             }
             $log.debug('Subscription found!');
             return sub;
-          }).catch(function(err) {
+          }).catch(function (err) {
             $log.debug(err);
             return Promise.reject(BrowserPushNotificationsStatus.FAILED_TO_SUBSCRIBE);
           });
         },
-        _notificationsSupported: function() {
+        _notificationsSupported: function () {
           return 'showNotification' in ServiceWorkerRegistration.prototype ? Promise.resolve() : Promise.reject(BrowserPushNotificationsStatus.NOTIFICATIONS_NOT_SUPPORTED);
         },
-        _pushMessagingSupported: function() {
+        _pushMessagingSupported: function () {
           return 'PushManager' in window ? Promise.resolve() : Promise.reject(BrowserPushNotificationsStatus.PUSH_NOT_SUPPORTED);
         },
-        register: function() {
+        register: function () {
           var q = $q.defer();
-          service._workersSupported().then(function() {
+          service._workersSupported().then(function () {
             $log.debug('Workers are supported');
             return service._registerWorker();
-          }).then(function() {
+          }).then(function () {
             $log.debug('Worker has been registered');
             return service._notificationsSupported();
-          }).then(function() {
+          }).then(function () {
             $log.debug('Notifications are supported');
             return service._pushMessagingSupported();
-          }).then(function() {
+          }).then(function () {
             $log.debug('Push is supported');
             return service._getPermissions();
-          }).then(function(permission) {
+          }).then(function (permission) {
             $log.debug('Permission: ', permission);
             return service._getServiceWorkerRegistration();
-          }).then(function(serviceWorkerRegistration) {
+          }).then(function (serviceWorkerRegistration) {
             $log.debug('Service registration: ', serviceWorkerRegistration);
             return service._getSubscription(serviceWorkerRegistration);
-          }).then(function(subscription) {
+          }).then(function (subscription) {
             $rootScope.$apply(q.resolve.bind(q, subscription));
-          }).catch(function(errorCode) {
+          }).catch(function (errorCode) {
             $rootScope.$apply(q.reject.bind(q, errorCode));
           });
           return q.promise;
         },
-        getSubscriptionId: function() {
-          return service.register().then(function(sub){
+        getSubscriptionId: function () {
+          return service.register().then(function (sub) {
             // 'PushSubscription.subscriptionId' is deprecated and is now included in 'PushSubscription.endpoint'. It will be removed in Chrome 45, around August 2015.
-            if(sub.subscriptionId) {
-              return sub.subscriptionId;
-            }
-            return sub.endpoint.split('/').pop();
+            $log.debug(sub);
+            // if(sub.subscriptionId) {
+            //   return sub.subscriptionId;
+            // }
+            // return sub.endpoint.split('/').pop();
+            return sub;
           });
         },
-        _registerWorker: function() {
+
+
+        _registerWorker: function () {
           // Check that it's at the root
-          if(workerUrl.lastIndexOf('/') > 0) {
+          if (workerUrl.lastIndexOf('/') > 0) {
             $log.warn('Worker url should be at the root of the domain');
           }
-          return navigator.serviceWorker.register(workerUrl).catch(function() {
+          return navigator.serviceWorker.register(workerUrl).catch(function () {
             return Promise.reject(BrowserPushNotificationsStatus.WORKER_REGISTRATION_FAIL);
           });
         },
-        _workersSupported: function() {
+        _workersSupported: function () {
           return 'serviceWorker' in navigator ? Promise.resolve() : Promise.reject(BrowserPushNotificationsStatus.WORKERS_NOT_SUPPORTED);
-        }
+        },
+        // Config methods
+        currentPermission: function () {
+          // Wrap with a function, so that we can extend this easily to
+          // support getting permissions using older API versions
+          return (Notification || {}).permission;
+        },
+        setWorkerUrl: function (u) {
+          workerUrl = u;
+        },
+        setUserVisibleOnly: function (direction) {
+          userVisibleOnly = direction;
+        },
       };
 
       return service;
     }];
   }
-).value('BrowserPushNotificationsStatus', {
-  WORKERS_NOT_SUPPORTED: 'workers_not_supported',
-  NOTIFICATIONS_NOT_SUPPORTED: 'notifications_not_supported',
-  PUSH_NOT_SUPPORTED: 'push_not_supported',
-  USER_BLOCKED_NOTIFICATIONS: 'user_blocked_notifications',
-  WORKER_REGISTRATION_FAIL: 'worker_registration_fail',
-  FAILED_TO_SUBSCRIBE: 'failed_to_subscribe'
-});
+  ).value('BrowserPushNotificationsStatus', {
+    WORKERS_NOT_SUPPORTED: 'workers_not_supported',
+    NOTIFICATIONS_NOT_SUPPORTED: 'notifications_not_supported',
+    PUSH_NOT_SUPPORTED: 'push_not_supported',
+    USER_BLOCKED_NOTIFICATIONS: 'user_blocked_notifications',
+    WORKER_REGISTRATION_FAIL: 'worker_registration_fail',
+    FAILED_TO_SUBSCRIBE: 'failed_to_subscribe'
+  });
